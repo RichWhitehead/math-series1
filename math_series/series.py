1e42af652f53083d0ee1d8a73349233f52e73192
Richard Whitehead <damian253@me.com>
2020-06-04 20:48:59 -0700

    added lucas

--- math_series/series.py
+++ math_series/series.py
@@ -8,3 +8,12 @@ def fibonacci(n):
   
   for n in range(1, 11):
     print(n, ":", fibonacci(n))
+    
+  
+    def lucas(n):
+      if n == 0:
+        return 2
+      elif n == 1:
+        return 1
+      else:
+        return lucas(n-1) + lucas (n-2)
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,5 +1,5 @@
 from math_series import __version__
-from math_series.series import fibonacci
+from math_series.series import fibonacci, lucas
 
 
 # Pass test
@@ -51,142 +51,93 @@ def test_fibonacci_fail_3():
         assert actual != expected
 
 
-# # Pass test
-# def test_fib_memo_pass_1(self):
-#         actual = fib_memo(3)
-#         expected = 2
-#         assert actual == expected
+
+# Pass test
+def test_lucas_memo_pass_1():
+        actual = lucas(3)
+        expected = 3
+        assert actual == expected
+
+# Pass test
+def test_lucas_memo_pass_2():
+        actual = lucas(7)
+        expected = 18
+        assert actual == expected
+
+Pass test
+def test_lucas_memo_pass_3():
+        actual = lucas(22)
+        expected = 24476
+        assert actual == expected
+
+Edge case
+def test_lucas_memo_pass_4():
+        actual = lucas(1)
+        expected = 2
+        assert actual == expected
+
+# Edge case
+def test_lucas_memo_pass_5():
+        actual = lucas(-1)
+        expected = 'Number must be greater that 0'
+        assert actual == expected
+
+# Fail case
+def test_lucas_memo_fail_1():
+        actual = lucas(3)
+        expected = 2
+        assert actual != expected
+
+# Fail case
+def test_lucas_memo_fail_2():
+        actual = lucas(-1)
+        expected = -1
+        assert actual != expected
+
+# Fail case
+def test_lucas_memo_fail_3():
+        actual = lucas(22)
+        expected = 22
+        assert actual != expected
+
+# Pass test
+def test_sum_series_pass_1():
+        actual = sum_series(3)
+        expected = 3
+        assert actual == expected
 
 # # Pass test
-# def test_fib_memo_pass_2(self):
-#         actual = fib_memo(7)
-#         expected = 13
-#         assert actual == expected
-
-# # Pass test
-# def test_fib_memo_pass_3(self):
-#         actual = fib_memo(22)
-#         expected = 17711
-#         assert actual == expected
-
-# # Edge case
-# def test_fib_memo_pass_4(self):
-#         actual = fib_memo(0)
-#         expected = 0
-#         assert actual == expected
-
-# # Edge case
-# def test_fib_memo_pass_5(self):
-#         actual = fib_memo(-1)
-#         expected = 'Number must be positive'
-#         assert actual == expected
-
-# # Fail case
-# def test_fib_memo_fail_1(self):
-#         actual = fib_memo(3)
-#         expected = 3
-#         assert actual != expected
-
-# # Fail case
-# def test_fib_memo_fail_2(self):
-#         actual = fib_memo(-1)
-#         expected = -1
-#         assert actual != expected
-
-# # Fail case
-# def test_fib_memo_fail_3(self):
-#         actual = fib_memo(22)
-#         expected = 22
-#         assert actual != expected
-
-
-# # Pass test
-# def test_lucas_memo_pass_1(self):
-#         actual = lucas_memo(3)
-#         expected = 3
-#         assert actual == expected
-
-# # Pass test
-# def test_lucas_memo_pass_2(self):
-#         actual = lucas_memo(7)
-#         expected = 18
-#         assert actual == expected
-
-# # Pass test
-# def test_lucas_memo_pass_3(self):
-#         actual = lucas_memo(22)
-#         expected = 24476
-#         assert actual == expected
-
-# # Edge case
-# def test_lucas_memo_pass_4(self):
-#         actual = lucas_memo(1)
-#         expected = 2
-#         assert actual == expected
-
-# # Edge case
-# def test_lucas_memo_pass_5(self):
-#         actual = lucas_memo(-1)
-#         expected = 'Number must be greater that 0'
-#         assert actual == expected
-
-# # Fail case
-# def test_lucas_memo_fail_1(self):
-#         actual = lucas_memo(3)
-#         expected = 2
-#         assert actual != expected
-
-# # Fail case
-# def test_lucas_memo_fail_2(self):
-#         actual = lucas_memo(-1)
-#         expected = -1
-#         assert actual != expected
-
-# # Fail case
-# def test_lucas_memo_fail_3(self):
-#         actual = lucas_memo(22)
-#         expected = 22
-#         assert actual != expected
-
-
-# # Pass test
-# def test_sum_series_pass_1(self):
-#         actual = sum_series(3)
-#         expected = 3
-#         assert actual == expected
-
-# # Pass test
-# def test_sum_series_pass_2(self):
+# def test_sum_series_pass_2():
 #         actual = sum_series(7)
 #         expected = 21
 #         assert actual == expected
 
 # # Pass test
-# def test_sum_series_pass_3(self):
+# def test_sum_series_pass_3():
 #         actual = sum_series(22)
 #         expected = 28657
 #         assert actual == expected
 
 # # Edge case
-# def test_sum_series_pass_4(self):
+# def test_sum_series_pass_4():
 #         actual = sum_series(1)
 #         expected = 1
 #         assert actual == expected
 
 # # Fail case
-# def test_sum_series_fail_1(self):
+# def test_sum_series_fail_1():
 #         actual = sum_series(3)
 #         expected = 2
 #         assert actual != expected
 
 # # Fail case
-# def test_sum_series_fail_2(self):
+# def test_sum_series_fail_2():
 #         actual = sum_series(-1)
 #         expected = -1
 #         assert actual != expected
 
 # # Fail case
-# def test_sum_series_fail_3(self):
+# def test_sum_series_fail_3():
 #         actual = sum_series(22)
 #         expected = 22
 #         assert actual != expected
